5cc0c08ccece02361fd22a65789b8c0bc9dee10a
dhruvaparnathi <dhruv.aparnathi.1@gmail.com>
2026-06-05 22:01:02 +0530

    deployment 5

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -24,7 +24,7 @@ app.use('/api/song', songRouter);
 
 const frontendPath = path.join(__dirname, '../public/dist');
 app.use(express.static(frontendPath));
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
